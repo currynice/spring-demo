0adc1f06042109a7584bc0d0731f9953b96cb0f8
chengxinyu-surface <694975984@qq.com>
2021-08-02 17:51:27 +0800

    HierarchicalBeanFactoryDemo

---
+++ spring-beans/src/main/java/org/cxy/springdemo/bean/beanFactory/hierarchical_bean_factory/HierarchicalBeanFactoryDemo.java
@@ -0,0 +1,19 @@
+package org.cxy.springdemo.bean.beanFactory.hierarchical_bean_factory;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * Description:   </br>
+ * Date: 2021/8/2 17:35
+ *
+ * @author :cxy </br>
+ * @version : 1.0 </br>
+ */
+public class HierarchicalBeanFactoryDemo {
+
+    public static void main(String[] args) {
+
+
+    }
+}
