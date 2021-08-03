df4e99cb5390fa67a819c47777005ea3b2475763
chengxinyu-surface <694975984@qq.com>
2021-08-03 13:56:30 +0800

    ConfigurableBeanFactoryDemo

---
+++ spring-beans/src/main/java/org/cxy/springdemo/bean/beanFactory/configurable_bean_factory/ConfigurableBeanFactoryDemo.java
@@ -0,0 +1,15 @@
+package org.cxy.springdemo.bean.beanFactory.configurable_bean_factory;
+
+/**
+ * Description: ConfigurableBeanFactory todo   </br>
+ * Date: 2021/8/3 13:36
+ *
+ * @author :cxy </br>
+ * @version : 1.0 </br>
+ */
+public class ConfigurableBeanFactoryDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
